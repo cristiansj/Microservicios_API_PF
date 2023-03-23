7b22bb681207be2270e9c16a10befe008e6e3e8e
cristian Solarte <cristian.solartej@uqvirtual.edu.co>
2023-03-23 10:08:11 -0500

    proyecto

---
+++ src/main/java/co/edu/uniquindio/Microservicios_API_PF/Microservicios_API_PFRestApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.Microservicios_API_PF;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class Microservicios_API_PFRestApplication extends Application {
+}
