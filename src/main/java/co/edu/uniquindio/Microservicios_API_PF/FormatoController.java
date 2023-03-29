44a9d4efed8206529a24b4f8c63fb7da8327014d
cristian Solarte <cristian.solartej@uqvirtual.edu.co>
2023-03-29 13:48:03 -0500

    Se arreglo el nombre de una clase y un error en ella

--- src/main/java/co/edu/uniquindio/Microservicios_API_PF/FormatoController.java
+++ src/main/java/co/edu/uniquindio/Microservicios_API_PF/FormatoController.java
@@ -7,14 +7,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import java.text.NumberFormat;
 
 /**
  *
  */
 @Path("/formato")
 @Singleton
-public class HelloController {
+public class FormatoController {
 
     @GET
     @Path("id_pedido/{id_pedido}")
